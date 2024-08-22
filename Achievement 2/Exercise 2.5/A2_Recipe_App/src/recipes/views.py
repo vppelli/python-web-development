44473e046224e1c8982943f4080301137eb1a07c
Michael Pietroski <mpworkplace@gmail.com>
2024-08-22 15:37:54 -0400

    Added Exercise 2.5 to Achievement 2 "Test commit"

---
+++ Achievement 2/Exercise 2.5/A2_Recipe_App/src/recipes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Recipe
+
+# Create your views here.
+def home(request):
+   return render(request, 'recipes/recipes_home.html')
+
+class RecipeListView(ListView):
+   model = Recipe
+   template_name = 'recipes/recipes_list.html'
+
+class RecipeDetailView(DetailView):
+   model = Recipe
+   template_name = 'recipes/recipes_detail.html'
